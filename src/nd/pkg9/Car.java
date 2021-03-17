ab409dc5577745ae6254a1a4071ed28f947bf05f
Vadim M <vadim.mozeiko@gmail.com>
2021-03-17 21:29:35 +0200

    Update Car.java

--- src/nd/pkg9/Car.java
+++ src/nd/pkg9/Car.java
@@ -1,43 +1,41 @@
 package nd.pkg9;
 
-
 public class Car {
-    
+
     String carName;
     int maxSpeed;
     int currentSpeed = 0;
     int distance = 0;
-    
+
     public Car(String name, int maxSpeed) {
         this.carName = name;
         this.maxSpeed = maxSpeed;
         this.greeting();
     }
-    
-    public void greeting(){
+
+    public void greeting() {
         System.out.println("Car " + this.carName + ", milage - " + distance + ", max speed - " + maxSpeed + ".");
 
     }
-    
-    public void accelerate(int value){
-        if (this.currentSpeed + value > this.maxSpeed){
+
+    public void accelerate(int value) {
+        if (this.currentSpeed + value > this.maxSpeed) {
             this.currentSpeed = this.maxSpeed;
-            return;
         } else {
-        this.currentSpeed += value;
+            this.currentSpeed += value;
         }
     }
-    
-    public void slowDown(int value){
+
+    public void slowDown(int value) {
         if (this.currentSpeed - value <= 0) {
             this.currentSpeed = 0;
-            return;
+        } else {
+            this.currentSpeed -= value;
         }
-        this.currentSpeed -=value;
     }
-    
-    public void drive(){
+
+    public void drive() {
         this.distance += this.currentSpeed;
-        System.out.println("Driving.." + " Current distance: " + this.distance + ", current speed: " + this.currentSpeed);
+        System.out.println("Driving.. " + this.carName +" Current distance: " + this.distance + ", current speed: " + this.currentSpeed);
     }
 }
